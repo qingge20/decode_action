82aab05575de2504ca29c30268c1533041fd508e
action <action@github.com>
2024-10-09 12:52:57 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,69 +1,150 @@
-//Wed Oct 09 2024 12:51:49 GMT+0000 (Coordinated Universal Time)
+//Wed Oct 09 2024 12:52:57 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
+$.leaders = [];
+$.finishCount = 0;
 $.version = "v1.0.0";
 $.logic = async function () {
   if (!$.superVersion) throw new Error("请更新脚本");
   if (!$.activityId || !$.activityUrl) {
-    $.expire = true;
     $.putMsg("activityId|activityUrl不存在");
+    $.expire = true;
     return;
   }
   $.UA = $.ua();
-  let i1lllIll = await $.isvObfuscator();
-  if (i1lllIll.code !== "0") {
-    $.putMsg("获取Token失败");
-    return;
-  }
-  if (["10044"].includes($.activityType)) {
-    await $.login();
-    let il1Il1I1 = await $.api("api/task/votePolitely/activity", {});
-    if (il1Il1I1.data.voteSelectList.length == 0) {
-      let iIIill = [il1Il1I1.data.rule[0].text];
-      il1Il1I1.data.votingForm == 2 && iIIill.push(il1Il1I1.data.rule[1].text);
-      let l1lilII1 = await $.api("api/task/votePolitely/savePkResult", {
-        "selectVote": iIIill
-      });
-      $.log("投票结果", l1lilII1);
-      if (l1lilII1.resp_code == 0) {}
-    }
-    for (let IliIil1 = 0; IliIil1 < 2; IliIil1++) {
-      let Il111I1I = await $.api("/api/prize/draw", {
-        "actId": $.activityId
-      });
-      if (Il111I1I.resp_code != 0) {
-        if (Il111I1I.resp_msg.includes("没有抽奖次数")) break;
-        $.putMsg("" + Il111I1I.resp_msg);
-        await $.wxStop(Il111I1I.resp_msg);
-        return;
-      } else $.putMsg("" + (Il111I1I.data?.["prizeName"] || "空气")), Il111I1I.data?.["prizeType"] === 3 && ($.prizeName = Il111I1I.data?.["prizeName"], $.addressId = Il111I1I.data?.["addressId"], await $.saveAddress());
+  let I11i11li = await $.isvObfuscator();
+  if (I11i11li.code !== "0") {
+    if ($.index < 20) $.putMsg("获取Token失败");else {
+      $.log("获取Token失败");
     }
     return;
   }
-  $.Token = i1lllIll?.["token"];
+  $.Token = I11i11li?.["token"];
   await $.getSimpleActInfoVo();
-  if ($.expire) return;
-  await $.getMyPing();
-  if (!$.Pin) {
+  if ($.expire) {
     return;
   }
+  await $.getMyPing();
+  if (!$.Pin) return;
   await $.accessLog();
-  let ilI1iIIl = await $.api("faxian/wxVote/activityContent", {
-    "activityId": $.activityId,
-    "pin": $.Pin
-  });
-  if (!ilI1iIIl.result || !ilI1iIIl.data) {
-    $.putMsg(ilI1iIIl.errorMessage || "活动已结束");
+  let l1illI11 = await $.api("wxUnPackingActivity/activityContent", "activityId=" + $.activityId + "&buyerNick=" + $.Pin + "&friendUuid=" + ($.friendUuid || ""));
+  if (!l1illI11.result || !l1illI11.data) {
+    $.putMsg(l1illI11.errorMessage);
+    await $.wxStop(l1illI11.errorMessage);
     return;
+  }
+  $.needCount = l1illI11.data.wucvo.unpackingPeople;
+  $.actStartTime = l1illI11.data.wucvo.startTime + " 00:00:00";
+  $.actEndTime = l1illI11.data.wucvo.endTime + " 23:59:59";
+  $.content = l1illI11.data.wdifo;
+  $.type = l1illI11.data.wdifo.type;
+  if ($.actStartTime > $.timestamp()) {
+    $.putMsg("活动未开始");
+    $.expire = true;
+    return;
+  }
+  if ($.timestamp() > $.actEndTime) {
+    $.putMsg("活动已结束");
+    $.expire = true;
+    return;
+  }
+  if ($.type === 1 || $.type === 20 || $.content.name.includes("券") || $.type === 7 && (await $.wxAddressStop($.content.name))) {
+    $.putMsg("垃圾或领完");
+    this.expire = true;
+    return;
+  }
+  if ($.needCount > $.cookies.length) {
+    $.expire = true;
+    $.putMsg("ck太少了，需要" + $.needCount + "助力");
+    return;
+  }
+  $.mySelfId = l1illI11.data.wucvo.mySelfId;
+  if ($.leaders.length < leaderNum) {
+    $.leaders.push({
+      "token": $.Token,
+      "pin": $.Pin,
+      "index": $.index,
+      "mySelfId": $.mySelfId,
+      "finish": false
+    });
+    $.putMsg("队长");
+  }
+  $.hasCount = l1illI11.data.wuivo.hasUnpackingPeople;
+  $.finishCount === 0 && ($.finishCount = $.hasCount);
+  let i11il1l1 = $.leaders.filter(lIiI1Il => lIiI1Il.finish === false);
+  $.friendUuid = i11il1l1.length > 0 ? i11il1l1[0].mySelfId : "";
+  if ($.hasCount >= $.needCount) {
+    $.leaders.filter(iiIiII1i => iiIiII1i.mySelfId === $.friendUuid)[0].finish = true;
+    $.putMsg("助力已满");
+    return;
+  }
+  if ($.friendUuid === $.mySelfId) {
+    $.log("自己不能助力自己跳过");
+    return;
+  }
+  if (!$.friendUuid) {
+    $.expire = true;
+    $.log("已经没有队长了，不跑了");
+    return;
+  }
+  let II1ll1lI = await $.api("wxUnPackingActivity/getMyFriendInfo", "friendUuid=" + ($.friendUuid || ""));
+  if (!II1ll1lI.result) {
+    $.putMsg(II1ll1lI.errorMessage);
+    return;
+  }
+  let ill1IiiI = await $.api("wxUnPackingActivity/unpackingInfo", "activityId=" + $.activityId + "&friendUuid=" + ($.friendUuid || "") + "&mySelfUuid=" + $.mySelfId);
+  if (!ill1IiiI.result) {
+    $.putMsg(ill1IiiI.errorMessage);
+    return;
+  }
+  console.log(ill1IiiI.data.hasHelpUnpacking, ill1IiiI.data.hasHelper);
+  if (ill1IiiI.data.hasHelpUnpacking) {
+    $.log("已帮拆过");
+    return;
+  }
+  let ii111ili = await $.api("wxUnPackingActivity/unPacking", "activityId=" + $.activityId + "&friendUuid=" + ($.friendUuid || "") + "&mySelfId=" + $.mySelfId);
+  ii111ili.errorMessage.includes("会员") && ($.type === 16 || $.type === 9 || $.type === 6 || $.type === 7) && (await $.openCard(), await $.wait(1000, 2000), ii111ili = await $.api("wxUnPackingActivity/unPacking", "activityId=" + $.activityId + "&friendUuid=" + ($.friendUuid || "") + "&mySelfId=" + $.mySelfId));
+  await $.wxStop(ii111ili?.["errorMessage"]);
+  if (!ii111ili.result) {
+    $.putMsg(ii111ili?.["errorMessage"]);
+    return;
+  }
+  $.log(ii111ili.errorMessage);
+  console.log("xxx", $.leaders);
+  if (++$.finishCount >= $.needCount) {
+    $.leaders.filter(i1l1iil => i1l1iil.mySelfId === $.friendUuid)[0].finish = true;
+    let I1II1lIi = $.leaders.filter(iiIil1I1 => iiIil1I1.mySelfId === $.friendUuid)[0];
+    $.finishCount = 0;
+    $.cookie = $.cookies[I1II1lIi.index - 1];
+    $.username = decodeURIComponent($.cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1]);
+    $.putMsg("助力已满", $.getRemarks($.username));
+    console.log("满了");
+    if ($.type !== 7) {
+      return;
+    }
+    try {
+      let Ililll11 = await $.isvObfuscator();
+      if (Ililll11.code !== "0") {
+        $.putMsg("获取Token失败");
+        return;
+      }
+      $.Token = Ililll11?.["token"];
+      await $.api("customer/getMyPing", "userId=" + $.venderId + "&token=" + $.Token + "&fromType=APP");
+      $.Pin = I1II1lIi.pin;
+      await $.api("common/" + ($.domain.includes("cjhy") ? "accessLog" : "accessLogWithAD"), "venderId=" + $.venderId + "&code=" + $.activityType + "&pin=" + $.Pin + "&activityId=" + $.activityId + "&pageUrl=" + encodeURIComponent($.activityUrl) + "&subType=app&adSource=");
+      let i11lIi = await $.api("wxUnPackingActivity/drawPrize", "activityId=" + $.activityId + "&mySelfId=" + I1II1lIi.mySelfId);
+      i11lIi.errorMessage.includes("会员") && (await $.openCard(), await $.wait(2 * 1000, 5 * 1000), i11lIi = await $.api("wxUnPackingActivity/drawPrize", "activityId=" + $.activityId + "&mySelfId=" + I1II1lIi.mySelfId));
+      i11lIi.result && i11lIi.data && ($.addressId = i11lIi.data.addressId, $.prizeName = i11lIi.data.drawInfoName, await $.saveAddress());
+    } catch (ll1i1IIl) {
+      console.log(ll1i1IIl);
+    }
   }
 };
 $.after = async function () {
   try {
-    for (let IIiiIilI of $.prizeList || []) {
-      $.msg.push("    " + IIiiIilI.prizeName + " 剩" + IIiiIilI.leftNum + "份");
-    }
-  } catch (IIl1iIi) {
-    console.log(IIl1iIi);
+    $.msg.push("    " + $.content.name + " " + $.content?.["type"] + " 需要:" + $.needCount + "助力");
+  } catch (i1Iii1iI) {
+    console.log(i1Iii1iI);
   }
-  $.msg.push("export M_WX_VOTE_DRAW_URL=" + $.activityUrl);
+  $.msg.push("export M_WX_UNPACK_DRAW_URL=\"" + $.activityUrl + "\"");
 };
